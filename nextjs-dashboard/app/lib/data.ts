48fb09058dcc681389b044142e092fc69ccd6262
kangtaewan <bras1717@naver.com>
2023-12-03 12:29:13 +0900

    chapter9 - Streaming

--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -44,8 +44,12 @@ export async function fetchLatestInvoices() {
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
+		/*예시 코드*/
+/*
+		await new Promise((resolve) => setTimeout(resolve, 10000));
+*/
 
-    const latestInvoices = data.rows.map((invoice) => ({
+		const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
